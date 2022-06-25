a1df12a9f01c5e1bfde1891016b43e1d95f9da23
andna <a.n.d.n.a@yandex.ru>
2022-06-25 19:31:10 +0300

    048

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
-export default function name () {
-const name1 = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name1}!`);
+
+export default function name() {
+  const name1 = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name1}!`);
 }
