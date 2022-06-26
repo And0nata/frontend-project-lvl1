b1b53435a52be2725d6a974f8d142cffababaecc
andna <a.n.d.n.a@yandex.ru>
2022-06-26 20:05:11 +0300

    050

---
+++ bin/brain-even.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const name1 = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name1}!`);
+let correctAnswer = 0;
+function getRandomInt() {
+  return Math.floor(Math.random() * 100);
+};
+if(correctAnswer<3) {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let number = getRandomInt();
+  console.log(`Question: ${number()}`);
+  const answer = readlineSync.question('Your answer: ');
+  if((answer=='yes' && number%2==0)||(answer=='no' && number%2!=0)) {
+      correctAnswer++;
+      console.log('Correct!');
+  } else {
+      if(answer=='yes' && number%2!=0) {
+          console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.`)
+      }
+      if(answer=='no' && number%2==0) {
+          console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.`)
+      }
+     console.log(`Let's try again, ${name1}!`);
+  }
+console.log(`Congratulations, ${name1}!`)
